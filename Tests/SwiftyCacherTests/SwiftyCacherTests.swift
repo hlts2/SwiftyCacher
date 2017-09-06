36afb994845078e98504aea0302919a15c1afa31
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2017-09-06 23:49:21 +0900

    fix

--- Tests/SwiftyCacherTests/SwiftyCacherTests.swift
+++ Tests/SwiftyCacherTests/SwiftyCacherTests.swift
@@ -2,11 +2,58 @@ import XCTest
 @testable import SwiftyCacher
 
 class SwiftyCacherTests: XCTestCase {
-    func testExample() {
+    
+    func testSetCache() {
+        let cache = SwiftyCacher.instance
+        
+        for i in 0...100 {
+            cache.setWithExpire(key: i.description, value: i, expiration: Date())
+        }
+    }
+    
+    func testGetCache() {
+        let cache = SwiftyCacher.instance
+        
+        let (v, ok) = cache.get(key: "0")
+        if ok {
+            print(v!)
+        }
     }
 
+    func testGetAllCache() {
+        let cache  = SwiftyCacher.instance
+        let values = cache.getAll()
+        print(values)
+    }
 
+    func testDeleteCache() {
+        let cache = SwiftyCacher.instance
+        let ok    = cache.delete(key: "0")
+        print(ok)
+    }
+
+    func testDeleteExpiredCache() {
+        let cache = SwiftyCacher.instance
+        let _ = cache.deleteExpired()
+    }
+
+    func testDeleteIfExpiredCache() {
+        let cache = SwiftyCacher.instance
+        cache.deleteIfExpired(key: "9")
+    }
+
+    func testDeleteAllCache() {
+        let cache = SwiftyCacher.instance
+        cache.deleteAll()
+    }
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("testSetCache", testSetCache),
+        ("testGetCache", testGetCache),
+        ("testGetAllCache", testGetAllCache),
+        ("testDeleteCache", testDeleteCache),
+        ("testDeleteExpiredCache", testDeleteExpiredCache),
+        ("testDeleteIfExpiredCache", testDeleteIfExpiredCache),
+        ("testDeleteAllCache", testDeleteAllCache)
     ]
 }
