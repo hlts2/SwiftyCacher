2d7ca15a3be745c9788fba7347003c9969587501
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2017-09-10 23:50:37 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -4,4 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyCacher"
+
+    /*
+    dependencies: {
+        .Package(url: "git@github.com:hlts2/SwiftyDateHelper.git", majorVersion: 1)
+    }
+    */
 )
